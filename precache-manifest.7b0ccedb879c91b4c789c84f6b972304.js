52ffbf32ba123825948ad9c491b1501e6378102d
misspia <pia.leung@mediamonks.com>
2019-12-29 16:45:03 +0100

    Updates

---
+++ precache-manifest.7b0ccedb879c91b4c789c84f6b972304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1748ed363e3b57e6a60afb946229f44a",
+    "url": "/StickerWars/index.html"
+  },
+  {
+    "revision": "821bd4e4fd488137e6b3",
+    "url": "/StickerWars/static/js/2.ec445bb0.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/StickerWars/static/js/2.ec445bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9230f51eef599a3280c5",
+    "url": "/StickerWars/static/js/main.b57f3990.chunk.js"
+  },
+  {
+    "revision": "cd465468dbd1896a15c8",
+    "url": "/StickerWars/static/js/runtime-main.1b5453cf.js"
+  }
+]);
