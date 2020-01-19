47e07c870fd9e8f3a67ee4d8e8193c3457b35407
misspia <pia.leung@mediamonks.com>
2020-01-19 15:23:31 +0100

    Create login form component

--- src/components/common/TextInput/TextInput.styles.js
+++ src/components/common/TextInput/TextInput.styles.js
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import { Colors } from '../../../themes';
 
 export const Wrapper = styled.div`
-  padding: 0.5em 1em;
+  padding: 0.5em 0;
   box-sizing: border-box;
-  border-width: 0 0 1px 0;
+  border-width: 0 0 0.1em 0;
   border-style: solid;
-  border-color: ${Colors.black};
+  border-color: ${props => props.isFocused ? Colors.blue : Colors.black};
+
+  transition: all 0.3s;
+  ${props => props.customStyles}
 `;
 
 export const Input = styled.input`
@@ -14,6 +17,7 @@ export const Input = styled.input`
   height: 100%;
   border: 0;
   background-color: transparent;
+  color: inherit;
 
   &:focus {
     outline: none;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
-import { Fonts, Styles } from '../themes';
+import { Fonts, Styles, Colors } from '../themes';
 import { brandVariants, brandSizes } from '../constants/brand';
-import { PageWrapper, Brand, TextInput } from '../components/common'
+import { PageWrapper, Brand } from '../components/common'
+import LoginForm from '../components/LoginForm';
 
 const pageWrapperStyles = css`
   align-items: center;
@@ -15,19 +16,15 @@ const Title = styled.div`
   margin-bottom: 1em;
 `;
 
-const LoginForm = styled.form`
-  width: 20em;
+const ErrorMessage = styled.div`
+  margin-top: 1em;
+  color: ${Colors.red};
 `;
 
-const textInputStyle = css`
-  margin: 1em 0;
-  width: 100%;
-`;
+export default function LoginPage({
 
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
+}) {
+  const [authError, setAuthError] = useState('');
   return (
     <PageWrapper
       customStyles={pageWrapperStyles}
@@ -39,23 +36,18 @@ const LoginPage = () => {
       <Title>
         Login to Snapchat
       </Title>
-      <LoginForm>
-        <TextInput
-          value={username}
-          placeholder='username'
-          onChange={(e) => setUsername(e.target.value)}
-          customStyles={textInputStyle}
-        />
-        <TextInput
-          value={password}
-          placeholder='password'
-          password
-          onChange={(e) => setPassword(e.target.value)}
-          customStyles={textInputStyle}
-        />      
-      </LoginForm>
+      <LoginForm
+        onSubmit={(username, password) => (
+          submitForm(username, password)
+        )}
+      />
+      {
+        authError && <ErrorMessage>Invalid credentials</ErrorMessage>
+      }
     </PageWrapper>
   );
 }
 
-export default LoginPage;
+function submitForm() {
+  console.debug('submit form');
+}
